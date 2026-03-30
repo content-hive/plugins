e7df0d41692dd1ada00d2147ccf5077c0d4985d3
XaoflySho <shaoxiaof@hotmail.com>
2026-03-30 13:27:30 +0800

    Update request headers and improve trace ID extraction in Xiaohongshu parser

--- xiaohongshu/const.py
+++ xiaohongshu/const.py
@@ -18,8 +18,10 @@ URL_PATTERN = r'https?://(?:www\.)?xhslink\.com/o/\S+|https?://(?:www\.)?xiaohon
 STREAM_CODEC_PRIORITY = ("h264", "h265", "h266", "av1")
 
 REQUEST_HEADERS = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
-    "referer": "https://www.xiaohongshu.com/"
+    "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.4 Mobile/15E148 Safari/604.1",
+    "sec-Fetch-Site": "none",
+    "sec-Fetch-Mode": "navigate",
+    "sec-Fetch-Dest": "document"
 }
 
 JS_INVALID_TOKENS = r'\b(?:undefined|NaN|Infinity)\b'

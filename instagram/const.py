2f26cc63b8be452db64f76591956fdc66080b938
XaoflySho <shaoxiaof@hotmail.com>
2026-05-29 10:52:48 +0800

    [Instagram] Add Instagram parser plugin scaffold

---
+++ instagram/const.py
@@ -0,0 +1,9 @@
+"""Constants for Instagram parser plugin."""
+
+DOMAIN = "instagram"
+
+# Platform info
+PLATFORM_CODE = "ig"
+PLATFORM_NAME = "Instagram"
+PLATFORM_URL = "https://www.instagram.com/"
+PLATFORM_ICON = "https://raw.githubusercontent.com/content-hive/assets/main/IconSet/Instagram.png"
